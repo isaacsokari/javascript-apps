005ebda96da421f9abc18dc66f60b057369b6560
Isaac Sokari <isaacsokari@gmail.com>
2020-09-06 20:49:59 +0100

    added function to get neighbor cells and shuffled neighbor list

--- maze-beater/index.js
+++ maze-beater/index.js
@@ -1,7 +1,8 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = document.body.clientWidth > 600 ? 600 : 300,
-  height = width;
+const width = document.body.clientWidth > 600 ? 600 : 300;
+const height = width;
+const cells = 3;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -35,3 +36,57 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+// maze generation
+
+const shuffle = (arr) => {
+  let counter = arr.length;
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+
+  return arr;
+};
+
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const moveThroughCell = (row, column) => {
+  // if cell is visited, return
+  if (grid[row][column]) return;
+
+  // mark current cell as visited
+  grid[row][column] = true;
+
+  // assemble randomly ordered list of neighbor
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1],
+  ]);
+  // for each neighbor,
+  // check if the neighbor is out of bounds
+  // if the neighbour has been visiited, continue to next neighbour
+  // remove a wall from horizontals/verticals
+  // visit next cell
+};
+
+moveThroughCell(1, 1);
