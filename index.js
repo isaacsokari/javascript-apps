472108f7630cf4cfd73fb33fc9c2035e81c87cab
Isaac Sokari <isaacsokari@gmail.com>
2020-09-09 00:21:24 +0100

    switched to bodyparser from the custom function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -18,26 +18,7 @@ app.get('/', (req, res) => {
 `);
 });
 
-const bodyParser = (req, res, next) => {
-  if (req.method === 'POST') {
-    req.on('data', (data) => {
-      const parsed = data.toString('utf8').split('&');
-      const formData = {};
-
-      for (let pair of parsed) {
-        const [key, value] = pair.split('=');
-        formData[key] = value;
-      }
-
-      req.body = formData;
-      next();
-    });
-  } else {
-    next();
-  }
-};
-
-app.post('/', bodyParser, (req, res) => {
+app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
   console.log(req.body);
   res.send('<h1>Account Created</h1>');
 });
