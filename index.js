a363f82eddf814effba4d2d9b8fc1b743e2a31b5
Isaac Sokari <isaacsokari@gmail.com>
2020-09-08 12:54:48 +0100

    implemented an alternate solution using promises

--- index.js
+++ index.js
@@ -2,34 +2,20 @@
 
 const fs = require('fs');
 
-// const { lstat } = fs.promises;
+const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  const allStats = Array(filenames.length).fill(null);
-
   for (let name of filenames) {
-    const index = filenames.indexOf(name);
+    try {
+      const stats = await lstat(name);
 
-    fs.lstat(name, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-
-      allStats[index] = stats;
-
-      const ready = allStats.every((stat) => {
-        return stat;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    });
+      console.log(name, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
