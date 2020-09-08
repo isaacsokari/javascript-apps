59797081bd3d62c07b6af64324bdbfc9a9acb47a
Isaac Sokari <isaacsokari@gmail.com>
2020-09-08 12:45:30 +0100

    implemented first method to prevent nls displaying files in random order

--- index.js
+++ index.js
@@ -2,11 +2,34 @@
 
 const fs = require('fs');
 
+// const { lstat } = fs.promises;
+
 fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
     console.log(err);
-    // throw new Error('')
   }
 
-  console.log(filenames);
+  const allStats = Array(filenames.length).fill(null);
+
+  for (let name of filenames) {
+    const index = filenames.indexOf(name);
+
+    fs.lstat(name, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      allStats[index] = stats;
+
+      const ready = allStats.every((stat) => {
+        return stat;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+    });
+  }
 });
