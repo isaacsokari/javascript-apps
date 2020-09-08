9ddc2947d121a47129e2425c32220287ea06679c
Isaac Sokari <isaacsokari@gmail.com>
2020-09-08 21:32:18 +0100

    added check to ensure the file exists

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
+const program = require('caporal');
+const { start } = require('repl');
+const fs = require('fs');
 
-const debounce = (func, delay = 1000) => {
-  let timerId;
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
 
-  return (...args) => {
-    if (timerId) clearTimeout(timerId);
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file "${name}"`);
+    }
 
-    timerId = setTimeout((...args) => func.apply(null, args), delay);
-  };
-};
+    const start = debounce(() => {
+      console.log("starting user's program");
+    }, 100);
 
-chokidar
-  .watch('.', {
-    ignored: 'node_modules/*',
-  })
-  .on(
-    'add',
-    debounce(() => {
-      console.log('file(s) added');
-    }, 1000)
-  )
-  .on(
-    'change',
-    debounce(() => {
-      console.log('file(s) changed');
-    })
-  )
-  .on(
-    'unlink',
-    debounce(() => {
-      console.log('file(s) unlinked');
-    })
-  );
+    chokidar
+      .watch('.', {
+        ignored: 'node_modules/*',
+      })
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
