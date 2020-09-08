d190c16c424dbd82a02e2dca66df98808795381c
Isaac Sokari <isaacsokari@gmail.com>
2020-09-09 00:17:24 +0100

    added, post request functionality and added  body parser middleware

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hi there');
+  res.send(`<h1>hi there</h1>
+  <form method="post">
+  <input type="email" name="email" placeholder="Enter Email Address" />
+  <input type="password" name="password" placeholder="Enter password" />
+  <input
+    type="password"
+    name="passwordConfirm"
+    placeholder="Re-enter password"
+  />
+  <button type="submit">Sign Up</button>
+</form>
+`);
+});
+
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', (data) => {
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
+  res.send('<h1>Account Created</h1>');
 });
 
 app.listen(3000, () => {
