8210ac9aeb8b46b548d59179bbe07e0de619892d
Isaac Sokari <isaacsokari@gmail.com>
2020-09-09 00:24:12 +0100

    used app.use middleware to apply bodyparser in the app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send(`<h1>hi there</h1>
@@ -18,7 +19,7 @@ app.get('/', (req, res) => {
 `);
 });
 
-app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
   res.send('<h1>Account Created</h1>');
 });
