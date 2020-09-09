08a6e902a6ad844b40fd7714c8f93ae36a648081
Isaac Sokari <isaacsokari@gmail.com>
2020-09-09 19:36:10 +0100

    added compare password method to the class

--- repositories/users.js
+++ repositories/users.js
@@ -45,6 +45,14 @@ class UsersRepository {
     return secureRecord;
   }
 
+  async comparePasswords(saved, supplied) {
+    const [hashed, salt] = saved.split('.');
+
+    const hashedSuppliedBuffer = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuffer.toString('hex');
+  }
+
   async writeAll(records) {
     await fs.promises.writeFile(
       this.filename,
