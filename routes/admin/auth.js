80170bcec76d0b32d1ed9a4b9d25306e02d55ddd
Isaac Sokari <isaacsokari@gmail.com>
2020-09-09 21:06:57 +0100

    added and used express-validator to check the signup form and displayed errors on the console.

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -9,27 +11,39 @@ router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirm } = req.body;
+router.post(
+  '/signup',
+  [
+    check('email').trim().normalizeEmail().isEmail(),
+    check('password').trim().isLength({ min: 4, max: 20 }),
+    check('passwordConfirm').trim().isLength({ min: 4, max: 20 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  const existingUser = await usersRepo.getOneBy({ email });
+    console.log(errors);
 
-  if (existingUser) {
-    return res.send('Email in use');
+    const { email, password, passwordConfirm } = req.body;
+
+    const existingUser = await usersRepo.getOneBy({ email });
+
+    if (existingUser) {
+      return res.send('Email in use');
+    }
+
+    if (password !== passwordConfirm) {
+      return res.send('passwords do not match');
+    }
+
+    // create user to represent this person
+    const user = await usersRepo.create({ email, password });
+
+    // store the id in the users cookie
+    req.session.userId = user.id;
+
+    res.send('<h1>Account Created</h1>');
   }
-
-  if (password !== passwordConfirm) {
-    return res.send('passwords do not match');
-  }
-
-  // create user to represent this person
-  const user = await usersRepo.create({ email, password });
-
-  // store the id in the users cookie
-  req.session.userId = user.id;
-
-  res.send('<h1>Account Created</h1>');
-});
+);
 
 router.get('/signout', (req, res) => {
   req.session = null;
