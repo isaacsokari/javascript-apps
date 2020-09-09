77d8f5c5343a11402317585de3f26ea404bebded
Isaac Sokari <isaacsokari@gmail.com>
2020-09-09 14:56:49 +0100

    implemented getAll and create methods

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { url } = require('inspector');
 
 class UsersRepository {
   constructor(filename) {
@@ -13,6 +14,32 @@ class UsersRepository {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    );
+  }
+
+  async create(attrs) {
+    //
+    const records = await this.getAll();
+    records.push(attrs);
+
+    // write the updated records to the file.
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+  }
 }
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+
+  await repo.create({ email: 'test@example.com', password: 'password' });
+  const users = await repo.getAll();
+
+  console.log(users);
+};
+
+test();
