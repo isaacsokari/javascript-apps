5697362164e1280377d2fe1e6503caa261762828
Isaac Sokari <isaacsokari@gmail.com>
2020-09-09 19:25:45 +0100

    implemented salt+hash to store passwords

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const { url } = require('inspector');
 const crypto = require('crypto');
+const util = require('util');
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -28,12 +30,19 @@ class UsersRepository {
     // attach random id using crypto
     attrs.id = this.randomId();
 
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buff = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    const secureRecord = {
+      ...attrs,
+      password: `${buff.toString('hex')}.${salt}`,
+    };
+    records.push(secureRecord);
 
     // write the updated records to the file.
     await this.writeAll(records);
-    return attrs;
+    return secureRecord;
   }
 
   async writeAll(records) {
