f40c94da284e4e4e6eacfaba2e5dbaf1bffe2302
Isaac Sokari <isaacsokari@gmail.com>
2020-09-10 23:45:14 +0100

    added functionality to the products endpoint, it now displays all products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,13 +3,18 @@ const multer = require('multer');
 
 const productsRepo = require('../../repositories/products');
 const newProductsTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
 const { handleErrors } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', async (req, res) => {
+  const products = await productsRepo.getAll();
+
+  res.send(productsIndexTemplate({ products }));
+});
 
 router.get('/admin/products/new', (req, res) => {
   res.send(newProductsTemplate({}));
