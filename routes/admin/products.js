fafa520457b5e6d885d28e71633aafde031b1cf4
Isaac Sokari <isaacsokari@gmail.com>
2020-09-10 22:39:28 +0100

    done validation for the products form, added multer to handle file uploads

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const newProductsTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
-
-const app = express();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
 router.get('/admin/products/new', (req, res) => {
   res.send(newProductsTemplate({}));
 });
+
+router.post(
+  '/admin/products/new',
+  [requireTitle, requirePrice],
+  upload.single('image'),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    console.log(req.file, req.body);
+
+    if (!errors.isEmpty()) {
+    }
+
+    const { title, price, image } = req.body;
+
+    // await productsRepo.create({ title, price, image });
+    res.send('submitted');
+  }
+);
 
 module.exports = router;
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,7 +4,7 @@ const { getErrors } = require('../../helpers');
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      <form method="POST">
+      <form method="POST" enctype="multipart/form-data">
         <input placeholder="title" name="title" />
         <input placeholder="price" name="price" />
         <input type="file" name="image" />
