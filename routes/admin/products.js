fde06915e0e6f72b2ce8ceb3e9a174f91f3cb268
Isaac Sokari <isaacsokari@gmail.com>
2020-09-11 00:00:10 +0100

    added middleware to restrict access to some routes

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,23 +5,24 @@ const productsRepo = require('../../repositories/products');
 const newProductsTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', async (req, res) => {
+router.get('/admin/products', requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
 
   res.send(productsIndexTemplate({ products }));
 });
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(newProductsTemplate({}));
 });
 
 router.post(
   '/admin/products/new',
+  requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
   handleErrors(newProductsTemplate),
@@ -30,7 +31,7 @@ router.post(
     const { title, price } = req.body;
 
     await productsRepo.create({ title, price, image });
-    res.send('submitted');
+    res.redirect('/admin/products');
   }
 );
 
