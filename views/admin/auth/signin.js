a9481f4860de661285a7285585b82e9be93f3e30
Isaac Sokari <isaacsokari@gmail.com>
2020-09-10 17:12:15 +0100

    added css, modified signup, signin and layout files for styling

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,18 +1,30 @@
 const layout = require('../layout');
-
 const { getErrors } = require('../../helpers');
 
 module.exports = ({ errors }) => {
   return layout({
-    title: 'Sign In',
     content: `
-  <form method="post">
-    <input type="email" name="email" placeholder="Enter Email Address" />
-    ${getErrors(errors, 'email')}
-    <input type="password" name="password" placeholder="Enter password" />
-    ${getErrors(errors, 'password')}
-    <button type="submit">Sign In</button>
-  </form>
-  `,
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-one-quarter">
+            <form method="POST">
+              <h1 class="title">Sign in</h1>
+              <div class="field">
+                <label class="label">Email</label>
+                <input required class="input" placeholder="Email" name="email" />
+                <p class="help is-danger">${getErrors(errors, 'email')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password</label>
+                <input required class="input" placeholder="Password" name="password" type="password" />
+                <p class="help is-danger">${getErrors(errors, 'password')}</p>
+              </div>
+              <button class="button is-primary">Submit</button>
+            </form>
+            <a href="/signup">Need an account? Sign Up</a>
+          </div>
+        </div>
+      </div>
+    `,
   });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,25 +1,38 @@
 const layout = require('../layout');
-
 const { getErrors } = require('../../helpers');
 
 module.exports = ({ req, errors }) => {
   return layout({
-    title: 'Sign Up',
     content: `
-<h1>hi there, your id is ${req.session.userId}</h1>
-  <form method="post">
-  <input type="email" name="email" placeholder="Enter Email Address" />
-  ${getErrors(errors, 'email')}
-  <input type="password" name="password" placeholder="Enter password" />
-  ${getErrors(errors, 'password')}
-  <input
-    type="password"
-    name="passwordConfirm"
-    placeholder="Re-enter password"
-  />
-  ${getErrors(errors, 'passwordConfirm')}
-  <button type="submit">Sign Up</button>
-</form>
-`,
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-one-quarter">
+            <form method="POST">
+              <h1 class="title">Sign Up</h1>
+              <div class="field">
+                <label class="label">Email</label>
+                <input required class="input" placeholder="Email" name="email" />
+                <p class="help is-danger">${getErrors(errors, 'email')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password</label>
+                <input required class="input" placeholder="Password" name="password" type="password" />
+                <p class="help is-danger">${getErrors(errors, 'password')}</p>
+              </div>
+              <div class="field">
+                <label class="label">Password Confirmation</label>
+                <input required class="input" placeholder="Password Confirmation" name="passwordConfirmation" type="password" />
+                <p class="help is-danger">${getErrors(
+                  errors,
+                  'passwordConfirmation'
+                )}</p>
+              </div>
+              <button class="button is-primary">Submit</button>
+            </form>
+            <a href="/signin">Have an account? Sign In</a>
+          </div>
+        </div>
+      </div>
+    `,
   });
 };
