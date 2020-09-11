2276d10e47ddab46985eb8760b0869206192a5f8
Isaac Sokari <isaacsokari@gmail.com>
2020-09-11 13:11:00 +0100

    added external products route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
+
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
 
 const app = express();
 
@@ -14,11 +16,8 @@ app.use(
   })
 );
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
-
-app.get('/', (req, res) => {
-  res.redirect('/signin');
-});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
