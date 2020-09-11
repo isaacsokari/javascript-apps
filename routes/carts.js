fcadfe2a4b56255fe75099af328fe11cae1badd5
Isaac Sokari <isaacsokari@gmail.com>
2020-09-11 16:26:19 +0100

    calculated total price using cart items price and quantity

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cartShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -26,10 +28,25 @@ router.post('/cart/products', async (req, res) => {
   }
   await cartsRepo.update(cart.id, { items: cart.items });
 
-  res.send('added to cart');
+  // res.send('added to cart');
+  res.redirect('back');
 });
 
-router.get('/cart/products', async (req, res) => {});
+router.get('/cart', async (req, res) => {
+  if (!req.session.cartId) {
+    res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 
 // router.post('/cart/:id/delete', async (req, res) => {});
 
