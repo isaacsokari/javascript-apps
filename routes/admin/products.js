a43e36e35537e4983c4784bd9c4d24d619a53c9f
Isaac Sokari <isaacsokari@gmail.com>
2020-09-11 12:56:56 +0100

    added delete functionality to the products page

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -76,4 +76,10 @@ router.post(
   }
 );
 
+router.post('/admin/products/:id/delete', requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id);
+
+  res.redirect('/admin/products');
+});
+
 module.exports = router;
